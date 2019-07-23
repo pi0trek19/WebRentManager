c68fc15cb386ddfaa6f362e3f77994259656bb7c
pi0trek19 <piotrek.kacprzak@yahoo.com>
2019-07-23 12:40:39 +0200

    initial commit

---
+++ Models/Rent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebRentManager.Models
+{
+    public class Rent
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int ClientId { get; set; }
+        public int DriverId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal RentFee { get; set; }
+        public int DamageFee { get; set; }
+        public int MilageLimit { get; set; }
+        public decimal OverMilageFee { get; set; }
+        public bool TyresIncluded { get; set; }
+        public bool ServiceIncluded { get; set; }
+        public bool AssistanceIncluded { get; set; }
+        public bool ReplacementCarIncluded { get; set; }
+        public bool FinanceIncluded { get; set; }
+
+    }
+}
